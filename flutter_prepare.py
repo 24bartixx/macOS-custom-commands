1b46bf48e22aae022824057e66ab4d4aa38b5e00
24bartixx <jestgitbartix@gmail.com>
2025-03-06 11:47:07 +0100

    init: flutter_prepare macOS command

---
+++ flutter_prepare.py
@@ -0,0 +1,21 @@
+import os
+
+file_content = f"""
+# Flutter flutetr_prepare Terminal command
+flutter_prepare() {{
+     dart fix --apply && \\
+     dart format . && \\
+    flutter analyze
+}}
+"""
+
+shell_config_path = os.path.expanduser("~/.zshrc")
+
+try:
+    with open(shell_config_path, "a") as file:
+        file.write(file_content)
+
+    print(f"Success! You can now use flutter_prepare before git push of your Flutter project")
+
+except Exception as e:
+    print(f"An error occurred: {e}")
